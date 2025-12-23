1d9e934a5243412c2ef4e8815d83dfb2c8452096
nayde8824 <valemonteforte@gmail.com>
2025-12-23 15:31:58 +0100

    aggiunta ip

--- src/application/ApiConfig.java
+++ src/application/ApiConfig.java
@@ -2,5 +2,5 @@ package application;
 
 public final class ApiConfig {
     private ApiConfig() {}
-    public static final String BASE_URL = "http://localhost:8080";
+    public static final String BASE_URL = "http://154.46.187.160:1966";
 }
